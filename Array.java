a7c9d2303e7ba361d9beb39ac1c984c4083220ab
nargis7 <ronaldonargis786@gmail.com>
2025-06-11 10:39:17 +0530

    Added Array

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array{
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        long[] restaurantContacts = new long[3];
+        restaurantContacts [0] = 9992346725L; // Elements can be updated and accessed with the help of index
+        restaurantContacts [1] = 9992346726L;
+        restaurantContacts [2] = 9992346727L;
+        System.out.println(restaurantContacts [1]) ;// Accessing and displaying the element at the 1st index  
+
+    }
+}
