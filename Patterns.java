b12d48f2e4d8e5006dd72cd88c85a0a022e03ab6
nargis7 <ronaldonargis786@gmail.com>
2025-07-13 13:14:59 +0530

    pattern

---
+++ Patterns.java
@@ -0,0 +1,10 @@
+public class Patterns {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        // System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+        // System.out.println(" ");
+        // System.out.println("* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
